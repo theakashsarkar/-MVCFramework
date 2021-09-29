d409183d0e216aacaaabe3aaa97c2baed9ed6927
Akash sarkar <theakashsarkar7@gmail.com>
2021-09-30 04:51:23 +0600

    new code added

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
+
+use App\controllers\SiteController;
 use App\core\Application;
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-$app->router->get('/contact', 'contact');
-$app->router->post('/contact', function(){
-    return 'handling submit data';
-});
+$app->router->get( '/',    [SiteController::class,'home'] );
+$app->router->get( '/contact', [SiteController::class, 'contact'] );
+$app->router->post('/contact', [SiteController::class, 'handleContact'] );
 
 $app->run();
